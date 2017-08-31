867bc69e0ab1f6202ba6344f3ec969c7a481708a
zlodiak <prozaik81-2@yandex.ru>
2017-08-31 21:27:27 +0300

    fails rate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {  MdButtonModule, 
@@ -9,19 +10,24 @@ import {  MdButtonModule,
 import { AppComponent } from './app.component';
 import { ConvDialogComponent } from './conv-dialog/conv-dialog.component';
 
+import { RateService } from './services/rate.service';
+
 @NgModule({
   declarations: [
     AppComponent,
     ConvDialogComponent
   ],
   imports: [
+    HttpModule,
     MdInputModule,
     MdDialogModule,
     BrowserAnimationsModule,
     MdButtonModule,
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    RateService
+  ],
   bootstrap: [AppComponent],
   entryComponents: [ConvDialogComponent]
 })
--- src/app/conv-dialog/conv-dialog.component.ts
+++ src/app/conv-dialog/conv-dialog.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Response } from '@angular/http';
+
+import { RateService } from '../services/rate.service';
+
 
 @Component({
   selector: 'app-conv-dialog',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConvDialogComponent implements OnInit {
 
-  constructor() { }
+	private rate: Object = {};
+	private targetRates: Object = {};
+
+  constructor(private rateService: RateService) { }
 
   ngOnInit() {
-  }
+  	this.getRate();
+  }  
+
+  private getRate(): void {
+    this.rateService
+        .getRate()
+        .subscribe(data => {        						
+        						this.rate = JSON.parse(data._body);
+                    console.log(typeof data._body, data._body);
+                    console.log(typeof this.rate, this.rate);
+                    //console.log(this.rate.Date);
+                    //this.targetRates = this.rate.Valute.USD.Value;
+
+										/*for(var prop in this.rate) {
+										  if (!this.rate.hasOwnProperty(prop)) continue;
+										  console.log(prop, this.rate[prop]);
+										}*/
+
+
+                  }, 
+                  err => {
+                    console.log('err')         
+                  });    
+  };  
 
 }
